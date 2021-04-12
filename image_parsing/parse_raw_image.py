c4a39c609d2319b80b394b213e84d7d9b194b63c
Trinh Quang. Son <sontrinh2601uet@gmail.com>
2021-04-12 12:14:34 +0700

    What change do? - invert color - change to white background What next? - make image more smooth

--- image_parsing/parse_raw_image.py
+++ image_parsing/parse_raw_image.py
@@ -6,9 +6,7 @@ import os
 
 import PIL
 import PIL.ImageOps
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image, ImageEnhance
+from PIL import Image
 
 
 class RawImageParser(object):
@@ -18,39 +16,30 @@ class RawImageParser(object):
         self.width = width
         self.height = height
         self.rotate = rotate
-        self.result_folder = r'.\result\output.jpg'
+        self.result_folder = r'.\result\output.png'
 
     def parse_raw_to_image(self):
         if not os.path.exists(os.path.dirname(self.result_folder)):
             os.mkdir(os.path.join(os.path.dirname(self.result_folder)))
 
-        image = np.fromfile(self.path_file, dtype='int16', sep="")
-        image = image.reshape([self.height, self.width])
-        plt.imshow(image)
-        plt.imsave(self.result_folder, image)
+        # image = np.fromfile(self.path_file, dtype='int16', sep="")
+        # image = image.reshape([self.height, self.width])
+        # plt.imshow(image)
+        # plt.imsave(self.result_folder, image)
+
+        raw_data = open(self.path_file, 'rb').read()
+        img_size = (self.width, self.height)
+        img = Image.frombuffer("L", img_size, raw_data, "raw", "I;16", 0, -1)
+        img.save(self.result_folder)
 
     def start_image_processing(self):
-        image = Image.open(self.result_folder)
-        raw_image = image.rotate(self.rotate)
-        raw_image.save(self.result_folder)
-
         image = PIL.Image.open(self.result_folder)
         gray_image = PIL.ImageOps.grayscale(image)
         gray_image = PIL.ImageOps.equalize(gray_image)
+        gray_image = PIL.ImageOps.invert(gray_image)
         gray_image.save(self.result_folder)
-
-        # img = Image.open(self.result_folder)  # get image
-        # pixels = img.load() # create the pixel map
-        #
-        # for i in range(img.size[0]):  # for every pixel:
-        #     for j in range(img.size[1]):
-        #         if pixels[i, j] > 100:
-        #             pixels[i, j] = 170
-        #
-        # img.save(self.result_folder)
 
     def get_destination(self):
         target_image = PIL.Image.open(r".\data\destination.jpg")
         target_image = PIL.ImageOps.grayscale(target_image)
-        target_image = PIL.ImageOps.invert(target_image)
         target_image.save(r'.\result\destination.jpg')
